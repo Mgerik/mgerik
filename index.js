35a64aafdc8f9202d0de337c90ed7449e3cf987d
Mgerik <122942982+Mgerik@users.noreply.github.com>
2024-07-19 18:37:46 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+function enviarMensaje() {
+    var telefono = "351967176886";
+    var mensaje = "Hola, estoy interesado en (1) esta propiedad en Portugal.";
+    var url = "https://wa.me/" + telefono + "?text=" + encodeURIComponent(mensaje);
+    window.open(url, '_blank');
+}
+
+function enviarMensaje2() {
+    var telefono = "351967176886";
+    var mensaje = "Hola, estoy interesado en (2) esta propiedad en Portugal.";
+    var url = "https://wa.me/" + telefono + "?text=" + encodeURIComponent(mensaje);
+    window.open(url, '_blank');
+}
+function enviarMensaje3() {
+    var telefono = "351967176886";
+    var mensaje = "Hola, estoy interesado en (3) esta propiedad en Portugal.";
+    var url = "https://wa.me/" + telefono + "?text=" + encodeURIComponent(mensaje);
+    window.open(url, '_blank');
+}
+function enviarMensaje4() {
+    var telefono = "351967176886";
+    var mensaje = "Hola, estoy interesado en (4) esta propiedad en Portugal.";
+    var url = "https://wa.me/" + telefono + "?text=" + encodeURIComponent(mensaje);
+    window.open(url, '_blank');
+}
+function enviarMensaje5() {
+    var telefono = "351967176886";
+    var mensaje = "Hola, estoy interesado en (5) esta propiedad en Portugal.";
+    var url = "https://wa.me/" + telefono + "?text=" + encodeURIComponent(mensaje);
+    window.open(url, '_blank');
+}
+function enviarMensaje6() {
+    var telefono = "351967176886";
+    var mensaje = "Hola, estoy interesado en (6) esta propiedad en Portugal.";
+    var url = "https://wa.me/" + telefono + "?text=" + encodeURIComponent(mensaje);
+    window.open(url, '_blank');
+}
